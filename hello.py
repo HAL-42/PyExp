37b22de8d2b6ab1921ac81f3fad38da32c55dccd
Xiaobo Yang <735135352@qq.com>
2018-09-25 19:59:44 +0800

    Add some files

---
+++ hello.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Xiaobo Yang
+# @Date:   2018-09-23 19:50:15
+# @Last Modified by:   Xiaobo Yang
+# @Last Modified time: 2018-09-23 19:52:06
+from typing import Tuple
+
+a: Tuple[int, int, int] = (1, 2, 4)
+b: Tuple[int, int, int] = a
+print(b)
